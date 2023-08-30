1afe2bc4ddf14db82582cf110e8a03eff8cb6e88
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-08-30 12:57:51 +0200

    Clean up v1

--- third-party/cryptopp-cmake/cryptopp/CMakeFiles/cryptest.dir/__/__/cryptopp/fipsalgt.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-third-party/cryptopp-cmake/cryptopp/CMakeFiles/cryptest.dir/__/__/cryptopp/fipsalgt.cpp.obj: \
- C:\Users\Fisnik\Documents\GitHub\cryptoutils\third-party\cryptopp\fipsalgt.cpp
--- third-party/cryptopp-cmake/cryptopp/CMakeFiles/cryptopp.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../../lib/libcryptopp.a"
-)
