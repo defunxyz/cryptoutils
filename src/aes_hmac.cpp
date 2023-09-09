e2b08a6241ac869d7c4a3b9286bf2baa6cd5ecb0
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-09-09 10:23:48 +0200

    Updated CMAKELists.txt

--- src/aes_hmac.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) Fisnik. All rights reserved.
-#include <iostream>
-#include "common.h"
-
-std::string encrypt(std::string data, SecByteBlock& secret_key){}
-std::string decrypt(std::string& data, SecByteBlock& secret_key){}
-int main(int argc, char* argv[]){ return 0; }
